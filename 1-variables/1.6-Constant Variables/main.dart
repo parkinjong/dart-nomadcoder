ca55eaec0ab5d673056ca21a0c0d18c266ef51e6
InJongPark <parkinjong@gmail.com>
2023-07-20 23:48:49 +0900

    add dart 01 variable chapter.

--- 1-variables/1.6-Constant Variables/main.dart
+++ 1-variables/1.6-Constant Variables/main.dart
@@ -1,14 +1,4 @@
-// 변수를 먼저 선언하고 나중에 값을 지정할 수 있음
 void main() {
-  // 타입선언이나 final 앞에 붙여줄 수 있는 수식어
-  // 값을 넣지 않으면 null로 기본값이 들어가는데 late를 이용하여 나중에 값을 지정해줄 수 있다.
-  late String nico;
-  late final String nico1;
-  nico = 'nico';
-  nico1 = 'nico';
-
-  late final String nico2;
-  nico2 = 'nico';
-  print(nico2);
-
+    // 컴파일 할 떄 알 수 있는 값으로 final 처럼 값을 한번만 지정 가능.
+    const API = '121212';
 }
